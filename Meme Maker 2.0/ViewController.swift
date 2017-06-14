690393b969d835885005acf37551d1799290833d
Ankita Satpathy <ankita@Ankitas-MacBook-Air.local>
2017-06-14 08:49:29 +0530

    Initial Commit

---
+++ Meme Maker 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Meme Maker 2.0
+//
+//  Created by Ankita Satpathy on 14/06/17.
+//  Copyright © 2017 Ankita Satpathy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
